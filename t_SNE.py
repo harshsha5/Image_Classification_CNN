71c5637be1eecc3689942e265cf7ea069bacc235
harshsha5 <harshbitsp@gmail.com>
2020-03-02 16:44:06 -0500

    Fixing legend T_SNE viz

--- t_SNE.py
+++ t_SNE.py
@@ -30,7 +30,6 @@ def main():
     save_path = "runs/"+timestr+'/'
     writer = SummaryWriter(save_path)
 
-    # train_loader = utils.get_data_loader('voc', train=True, batch_size=args.batch_size, split='trainval',model_to_use = int(args.model_to_use))
     test_loader = utils.get_data_loader('voc', train=False, batch_size=args.test_batch_size, split='test',model_to_use = int(args.model_to_use))
 
     MODEL_PATH = "../Saved_Models/trained_CaffeNet"
@@ -48,20 +47,18 @@ def main():
     print('Average_Test_Loss: ', avg_test_loss)
 
     intermediate_output = torch.squeeze(fc7_output[0])
-    NUM_IMAGES_TO_MAKE_TSNE = 1000
+    NUM_IMAGES_TO_MAKE_TSNE = 11
     assert(int(args.test_batch_size)==NUM_IMAGES_TO_MAKE_TSNE)
-    # ipdb.set_trace()
+
     dataiter = iter(test_loader)
     images,labels,wgt = dataiter.next()
 
-    assert(labels.shape[0]==1000)   #Make sure that the test batch size is 1000
+    assert(labels.shape[0]==11)   #Make sure that the test batch size is 1000
 
     #Add t-SNE code here
     intermediate_output = intermediate_output.cpu().numpy()
     intermediate_output_embedded = manifold.TSNE(n_components=2).fit_transform(intermediate_output)
 
-    # colors = ["black","grey","lightcoral","red","saddlebrown","darkorange","gold","olivedrab","lawngreen","turquoise",
-    #             "deepskyblue","royalblue","blue","indigo","magenta","crimson","orangered","goldenrod","steelblue","springgreen"]
     colors = [(0.3,0.3,0.5),(0.1,0.2,0.3),(0.2,0.1,0.5),(0.1,0.6,0.6),(0.9,0.3,0.1),(0.8,0.2,0.2),(0.4,0.4,0.4),(0.1,0.7,0.6),(0.2,0.8,0.5),(0.1,0.6,0.1),
                 (0,0.1,0.4),(0.5,0.5,0),(0.6,0,0.5),(0.2,0.4,0.9),(0,0,0.9),(0.2,0.3,0.6),(0.9,0.5,0.5),(0.7,0.8,0.9),(0.2,0.1,0.1),(0.9,0.5,0.1)]
 
@@ -69,6 +66,7 @@ def main():
                    'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
                    'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
 
+    indices_covered = []
     for row in range(intermediate_output_embedded.shape[0]):
         im_label_vector = labels[row].cpu().numpy()
         positive_labels = np.argwhere(im_label_vector==1).flatten()
@@ -82,18 +80,15 @@ def main():
         i+=1   
         avg_r,avg_g,avg_b = total_r/i, total_g/i, total_b/i
         color_array = np.array([[avg_r,avg_g,avg_b ]])
-        # ipdb.set_trace()
-        # plt.scatter(intermediate_output_embedded[0], intermediate_output_embedded[1], c=(avg_r,avg_g,avg_b))
-        if(i==1):
+        if(i==1 and positive_labels[0] not in indices_covered):
             plt.scatter(intermediate_output_embedded[row][0], intermediate_output_embedded[row][1], c=color_array,label = CLASS_NAMES[positive_labels[0]])
+            indices_covered.append(positive_labels[0])
         else:
             plt.scatter(intermediate_output_embedded[row][0], intermediate_output_embedded[row][1], c=color_array)
-        plt.xlabel("Dim_1")
-        plt.ylabel("Dim_2")
-        plt.legend(loc='upper left')
-    # plt.legend((lo, ll, l, a, h, hh, ho),
-    #    ('Low Outlier', 'LoLo', 'Lo', 'Average', 'Hi', 'HiHi', 'High Outlier'),
-    #    scatterpoints=1,loc='upper right',title="Classes",ncol=4,fontsize=8)
+        plt.xlabel("x")
+        plt.ylabel("y")
+    plt.legend(loc='lower left', bbox_to_anchor= (0.0, 1.01), ncol=5, 
+            borderaxespad=0, frameon=False)
     plt.savefig("t-sne visualization")
     print("t-SNE visualization saved")
     plt.show()
